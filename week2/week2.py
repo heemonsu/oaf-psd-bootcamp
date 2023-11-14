540c3552f4abb884bc77c9a0516f767027e31538
Spiffy664 <spacemanspiff982@gmail.com>
2023-11-14 18:41:10 -0500

    Update week2.py
    added type annotation and camel case method names

--- week2/week2.py
+++ week2/week2.py
@@ -1,52 +1,51 @@
 class Animal():
 
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int)  -> None:
         self.name = name
         self.age = age
 
-    def who(self):
+    def who(self) -> None:
         print('name : ', self.name,  '\nage : ', self.age)
 
 class Cat(Animal):
-    def Speak(self):
+    def speak(self) -> None:
         print("I am a cat. Meow.")
         
 class Dog(Animal):
-    def Speak(self):
+    def speak(self) -> None:
         print("I am a dog. Woof.")
 
 class Zoo():
-    def __init__(self, list=[]):
+    def __init__(self, list: list=[]) -> None:
         self.list = list
 
-    def speak_all(self):
+    def speakAll(self) -> None: 
         for element in self.list:
-            element.Speak()
+            element.speak()
             element.who()
 
 
 class ZooKeeper():
-    def __init__(self, Zoo):
+    def __init__(self, Zoo: Zoo) -> None:
         self.Zoo = Zoo
     
-    def add_animal(self, Animal):
+    def addAnimal(self, Animal: Animal) -> None:
         self.Zoo.list.append(Animal)
             
 spiffy = Cat("spiffy", 32)
 spiffy.who()
-#spiffy.Speak()
+#spiffy.speak()
 pem = Dog("Pem", 40)
 pem.who()
-#pem.Speak()
+#pem.speak()
 list1 = [spiffy, pem] 
 zoo = Zoo(list1)
 zoo = Zoo(list=[spiffy, pem])
-# why can't we pass spiffy and pem to the zoo list like this... zoo = Zoo({spiffy, pem})
-#zoo.speak_all()
+
 
 zanimal = Cat("zanimal", 100)
 
 zooMan = ZooKeeper(zoo)
-zooMan.add_animal(zanimal)
+zooMan.addAnimal(zanimal)
 
-zoo.speak_all()
+zoo.speakAll()
