ceec9ab2349205b58177ed4c4b20f7a25436a183
Terry <erryte@yahoo.com>
2023-11-29 07:42:41 -0800

    add data folder, db

--- weather_app/weather_app.py
+++ weather_app/weather_app.py
@@ -1,14 +1,22 @@
-from abc import ABC, abstractmethod
 import requests
 import random
+import sqlite3
+
+from abc import ABC, abstractmethod
+from datetime import datetime, timedelta
 
 # for testing, use a constant seed
 random.seed(0)
 
 SERVICE_REFERENCE = "api"
+DB_FILE = "data/weather_database.db"
 
 MIN_TEMP = 0.0
 MAX_TEMP = 40.0
+
+# conn = sqlite3.connect('database.db')
+# conn.execute('CREATE TABLE accounts (username TEXT, password TEXT)')
+# conn.close()
 
 class AbstractWeatherService(ABC):
         @abstractmethod
@@ -69,6 +77,49 @@ class DataSourceHandler():
             # self.do_other_thing(resolved_recent_weather)
             self.visualization_handler.visualize_data(resolved_recent_weather)
 
+class WeatherDatabase:
+    def __init__(self, db_file):
+          self.conn = sqlite3.connect(db_file)
+          self.cursor = self.conn.cursor()
+          self.cursor.execute('''
+                CREATE TABLE IF NOT EXISTS weather (
+                              id INTEGER PRIMARY KEY AUTOINCREMENT,
+                              longitude REAL,
+                              latitude REAL,
+                              timestamp TEXT
+                )
+                              ''')
+    def reset_database(self):
+         self.cursor.execute('''
+            DELETE FROM weather
+                             ''')
+         self.conn.commit()
+         print("Database deleted!")
+    
+    def get_weather_data(self, weather_service, longitude, latitude):
+         self.cursor.execute('''
+            SELECT * FROM weather
+            WHERE longitude = ?
+            WHERE latitude = ?
+                             ''', (longitude, latitude,))
+         data = self.cursor.fetchall()
+
+         if data:
+              return data
+         else:
+              weather_data = weather_service.get_weather_data(longitude, latitude)
+
+              self.cursor.execute('''
+                INSERT INTO weather (longitude, latitude, temperature)
+                VALUES (?, ?, ?)
+                                  ''', (longitude, latitude, str(weather_data)))
+              self.conn.commit()
+
+              return weather_data
+         
+    def __del__(self):
+         self.conn.close()
+
 if __name__ == "__main__":
         # use a factory to build the service
         useable_service = ServiceFactory.buildService(SERVICE_REFERENCE)
@@ -79,4 +130,7 @@ if __name__ == "__main__":
         # use some data and call the program
         latitude = 37.7723281
         longitude = -122.4530167
+        # db = WeatherDatabase(DB_FILE)
+        # service = 
         data_handler.resolve_weather(longitude, latitude)
+        # weather_data = db.get_weather_data(service, longitude, latitude)
